6823c814f2478da7b61c838ff7bc7e2db4b447fa
Felipe <felipe.fsq@gmail.com>
2016-11-06 18:50:41 -0300

    configuration, validation and services

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +12,12 @@ namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -19,10 +27,33 @@ namespace TheWorld.Controllers.Web
         public IActionResult About()
         {
             return View();
-        } 
+        }
 
         public IActionResult Contact()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrWhiteSpace("email"))
+                {
+                    ModelState.AddModelError("", "Could not send email ");
+                }
+                if (_mailService.SendMail("",
+                                 "",
+                                 $"Contact page from {model.Name}, ({model.Email})",
+                                 model.Message
+                                 ))
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = "Mail Sent, Thanks!";
+                }
+            }
+
             return View();
         }
     }
